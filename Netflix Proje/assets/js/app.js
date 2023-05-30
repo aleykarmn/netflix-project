74b11887c16a172fc2b81c817feb18a7dab90813
Aleyna <110481605+aleykarmn@users.noreply.github.com>
2023-05-30 22:30:04 +0300

    Add files via upload

---
+++ Netflix Proje/assets/js/app.js
@@ -0,0 +1,56 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".mySwiper-3", {
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".mySwiper-4", {
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".mySwiper-5", {
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".mySwiper-6", {
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".mySwiper-7", {
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
